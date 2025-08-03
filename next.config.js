56ceed341cb70c90da80b7e4aa0ac33bd82d1e8b
Kwame Nkrumah <122077830+nkkwame@users.noreply.github.com>
2025-08-04 04:18:29 +0700

    Configure for root domain deployment

--- next.config.js
+++ next.config.js
@@ -10,8 +10,6 @@ const nextConfig = {
         unoptimized: true,
         domains: ['images.unsplash.com', 'via.placeholder.com'],
     },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/nkkwame' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/nkkwame' : '',
 }
 
 module.exports = nextConfig
